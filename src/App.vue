bf73f5f57b8ff695654fa6df55d9274c107c2767
brunogeek9 <brunogeek9@gmail.com>
2019-07-07 09:50:23 -0300

    implementação inicial do componente todolist

--- src/App.vue
+++ src/App.vue
@@ -33,6 +33,7 @@ export default {
   /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
+  font-size: 22px;
 }
 .logo{
   display: block;
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,10 +3,30 @@
       <input type="text" class="todo-input" placeholder="o que precisa ser feito ?"
       v-model="newTodo" @keyup.enter="addTodo">
       <!-- lista de coisas para fazer -->
-      <div v-for="todo in todos" :key="todo.id" class="todo-item">
-          {{todo.title}}
+      <div v-for="(todo,index) in todos" :key="todo.id" class="todo-item">
+        <div class="todo-item-left">
+            <input type="checkbox" v-model="todo.completed">
+            <div v-if="!todo.editing" @dblclick="editTodo(todo)" class="todo-item-label" 
+            :class="{completed : todo.completed}"> 
+                {{todo.title}} 
+            </div>
+            <input v-else class="todo-item-edit" type="text" v-model="todo.title" 
+            @blur="doneEdit(todo)" @keyup.enter="doneEdit(todo)" @keyup.esc="cancelEdit(todo)" v-focus>
+        </div>
+        
+        <div class="remove-item" @click="removeTodo(index)">
+            &times;
+        </div>
       </div>
       
+      <div id="extra-container">
+        <div>
+            <label>
+                <input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos()"> Limpar Todos
+            </label>
+        </div>
+        <div>{{remaining}} items sobrando</div>
+      </div>
 
   </div>
 </template>
@@ -18,11 +38,19 @@ export default {
       return{
           newTodo: '',
           idForTodo: 3,
+          editCache: '',
           todos:[
-              {'id': 1,'title':'terminar o tutorial','completed': false},
-              {'id': 2,'title':'lavar o resto da roupa','completed': false}
+              {'id': 1,'title':'terminar o tutorial','completed': false, 'editing': false},
+              {'id': 2,'title':'lavar o resto da roupa','completed': false, 'editing': false}
           ]
       }
+  },
+  directives: {
+    focus: {
+      inserted: function (el) {
+        el.focus()
+      }
+    }
   },
   methods:{
       addTodo(){
@@ -36,22 +64,118 @@ export default {
           });
           this.newTodo = '';
           this.idForTodo++;
+      },
+      removeTodo(index){
+          this.todos.splice(index,1);
+      },
+      editTodo(todo){
+          this.editCache = todo.title;
+          todo.editing = true;
+      },
+      doneEdit(todo){
+          if(todo.title.trim().length == ''){
+              todo.title = this.editCache;
+          }
+          todo.editing = false;
+
+      },
+      cancelEdit(todo){
+          todo.title = this.editCache;
+          todo.editing = false;
+      },
+      checkAllTodos(){
+          this.todos.forEach((todo) => todo.completed = event.target.checked);
+      }
+  },
+  computed: {
+      remaining(){
+          return this.todos.filter(todo => !todo.completed).length;
+      },
+      anyRemaining(){
+          return this.remaining != 0;
       }
   }
 }
 </script>
 
-<style>
-    .todo-input{
-        width: 100%;
-        padding: 10px 18px;
-        font-size: 18px;
-        margin-bottom: 10px;
+<style lang="scss">
+      @import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+    &:focus {
+      outline: 0;
     }
-    .todo-item{
-        margin-bottom: 12px;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
+  }
+  .todo-item {
+    margin-bottom: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    animation-duration: 0.3s;
+  }
+  .remove-item {
+    cursor: pointer;
+    margin-left: 14px;
+    &:hover {
+      color: black;
     }
+  }
+  .todo-item-left { // later
+    display: flex;
+    align-items: center;
+  }
+  .todo-item-label {
+    padding: 10px;
+    border: 1px solid white;
+    margin-left: 12px;
+  }
+  .todo-item-edit {
+    font-size: 24px;
+    color: #2c3e50;
+    margin-left: 12px;
+    width: 100%;
+    padding: 10px;
+    border: 1px solid #ccc; //override defaults
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    &:focus {
+      outline: none;
+    }
+  }
+  .completed {
+    text-decoration: line-through;
+    color: grey;
+  }
+  .extra-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 16px;
+    border-top: 1px solid lightgrey;
+    padding-top: 14px;
+    margin-bottom: 14px;
+  }
+  button {
+    font-size: 14px;
+    background-color: white;
+    appearance: none;
+    &:hover {
+      background: lightgreen;
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+  .active {
+    background: lightgreen;
+  }
+  // CSS Transitions
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .2s;
+  }
+  .fade-enter, .fade-leave-to {
+    opacity: 0;
+  }
 </style>
